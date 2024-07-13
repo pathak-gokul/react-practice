d48c5f2050389aaa3d85fe6cc9ce80bab1afb6a5
Gokul Pathak <gokul_pathak@yahoo.com>
2024-07-13 19:03:36 +0545

    feat: add simple welcome message component

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message() {
+    //jsx Javascript XML
+    const name = 'Alan';
+    if(name)
+        return <h2>Welcome to the website {name}.</h2>;
+    return <h2>Welcome to the website User:). </h2>;
+}
+
+export default Message;
