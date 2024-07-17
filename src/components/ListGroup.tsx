0dd60f680ad94ca6a223b6ffd6c89222b96c6503
Gokul Pathak <gokul_pathak@yahoo.com>
2024-07-17 20:45:44 +0545

    feat: add conditional rendering for empty ListGroup
      - Added conditional rendering to display a message when no items are
        found
      - Used logical AND (&&) operator for concise conditional rendering
      - Maintained existing structure and styling of ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,23 @@
 function ListGroup() {
-  const Items = ["Nepal", "India", "America", "Switerland", "Germany"];
+  let Items = ["Nepal", "India", "America", "Switerland", "Germany"];
+  Items = [];
+
+  // const getMessage = () => {
+  //   return Items.length === 0 ? <p>No items found.</p> : Null;
+  // };
+
+  // if (Items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>;
+  //     </>
+  //   );
   return (
     <>
       <h1>List Group</h1>
+      {/* {Items.length === 0 ? <p>No items found.</p> : Null} */}
+      {Items.length === 0 && <p>No items found.</p>}
       <ul className="list-group">
         {Items.map((item) => (
           <li key={item}>{item}</li>
