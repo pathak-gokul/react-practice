051cf8812755e2323955d4116a77d8ce785552c3
Gokul Pathak <gokul_pathak@yahoo.com>
2024-07-21 14:28:37 +0545

    feat: implement dismissible alert on button click
    	- Created a reusable Button component
    	- Created a dismissible Alert component
    	- Implemented state management to show and hide Alert on button click
    	- Enhanced user experience with dismissible alert functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,28 @@
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
+import { useState } from "react";
 function App() {
   let items = ["Nepal", "India", "America", "Switerland", "Germany"];
   // return <div><Message/></div>
   //props: input passed  to a componenet similar to function args immutable
   //state data managed by a componeent similar to local variable mutable
+  const [alertVisible, setAlertVisiblity] = useState(false);
   const handleSelectedItem = (item: string) => {
     console.log(item);
   };
   return (
     <div>
-      <div className="alert alert-primary">
-        <Alert>
-          Cities fetched successfully!! <span>gp</span>
-        </Alert>
-      </div>
+      {alertVisible && (
+        <div>
+          <Alert onClose={() => setAlertVisiblity(false)}>
+            Cities fetched successfully!!
+          </Alert>
+        </div>
+      )}
       <div>
-        <Button onClick={() => console.log("clicked")}>My Button</Button>
+        <Button onClick={() => setAlertVisiblity(true)}>My Button</Button>
+        {/* <Button onClick={() => console.log("clicked")}>My Button</Button> */}
       </div>
       <ListGroup
         items={items}
