776c3c21959133f7041268513d617071391a28c8
Wow gokul <gokul.pathak@wowfinstacknepal.com>
2024-09-03 17:42:15 +0545

    feat: npm & prettier setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 const Pet = (props) => {
-    return React.createElement("div",{},
-    [
-        React.createElement("h1",{},props.sound),
-        React.createElement("h2",{}, props.gg),
-        React.createElement("h2",{},props.type),
-    ])
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.sound),
+    React.createElement("h2", {}, props.gg),
+    React.createElement("h2", {}, props.type),
+  ]);
 };
 const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { 
-            sound: 'meow',
-            gg: 'is',
-            type: 'cat'
-        }),
-        React.createElement(Pet, { 
-            sound: 'bau bau',
-            gg: 'is',
-            type: 'dog'
-        }),
-      ]
-    )
-  };
-  const container = document.getElementById('root');
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      sound: "meow",
+      gg: "is",
+      type: "cat",
+    }),
+    React.createElement(Pet, {
+      sound: "bau bau",
+      gg: "is",
+      type: "dog",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
