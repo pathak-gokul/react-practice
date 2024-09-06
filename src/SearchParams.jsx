2deba0ba9ca47d50bd311940fa333a5505906861
Wow gokul <gokul.pathak@wowfinstacknepal.com>
2024-09-06 16:18:18 +0545

    feat: adding animal breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -5,6 +5,8 @@ const Animals = ["elephant", "cat", "dog", "rat", "rabbit"];
 const SearchParams = () => {
   const [location, setLocation] = useState("Pokhara, NP");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["breed"];
   return (
     <div className="search-params">
       <form>
@@ -21,12 +23,32 @@ const SearchParams = () => {
           Animal
           <select
             id="animal"
-            value="{animal}"
-            onChange={(e) => setAnimal(e.target.value)}
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
           >
             <option />
             {Animals.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
