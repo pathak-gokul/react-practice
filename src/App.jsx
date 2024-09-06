7fdf4db1560f64d94c219356854f209b31a903b3
Wow gokul <gokul.pathak@wowfinstacknepal.com>
2024-09-06 15:32:28 +0545

    feat: hooks example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return (<div>
-    <h1>Adpot Me!</h1>
-   <Pet sound="bau bau" gg="is" type="dog"/>
-  </div>)
+  return (
+    <div>
+      <h1>Adpot Me!</h1>
+      <Pet sound="bau bau" gg="is" type="dog" />
+      <Pet sound="bau bau" gg="is" type="dog" />
+      <SearchParams />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,10 +1,10 @@
 const Pet = (props) => {
-    return(
-        <div>
-            <h1>{props.sound}</h1>
-            <h2>{props.gg}</h2>
-            <h2>{props.type}</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>{props.sound}</h1>
+      <h2>{props.gg}</h2>
+      <h2>{props.type}</h2>
+    </div>
+  );
+};
 export default Pet;
